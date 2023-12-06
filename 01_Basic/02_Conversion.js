7ec8661cf9df4a03d441fa953e791121c9420fe6
Abhishek Jain <123144040+abhishekjain96@users.noreply.github.com>
2023-12-06 13:34:12 +0000

    Discuss in Conversion

---
+++ 01_Basic/02_Conversion.js
@@ -0,0 +1,35 @@
+// Convert String to Number
+let name = "33"
+
+let nameinNumber = Number(name)
+console.log(nameinNumber);
+
+console.log(typeof nameinNumber);
+
+/*
+"33" => 33
+"sky" => NaN
+
+*/
+
+//Convert Number to Boolean
+
+let age = "abc";
+
+let ageinBoolean = Boolean(age);
+console.log(ageinBoolean)
+
+/*
+abc => true
+""  => false
+12  => true
+*/
+
+// Convert Boolean to Number
+
+let isLoggedin = undefined
+
+let numberisloogedin = Number(isLoggedin)
+
+console.log(numberisloogedin)
+console.log(typeof numberisloogedin)
