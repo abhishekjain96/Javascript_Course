16a7d262a2c724bce25d9e0c191e765c35dca761
Abhishek Jain <123144040+abhishekjain96@users.noreply.github.com>
2023-12-06 12:56:55 +0000

    discuss of variable

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,26 @@
+const accountId = 63911475
+let accountEmail = "jainabhishekbcamca@gmail.com"
+var accountPassword = 12345
+abc = 123
+
+// accountId = 123 never update because const keyword used not change
+
+// var not preffer to use because of block and function scope issue
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,abc])
+
+// Data Types in Javascript
+
+// number => 123 (range = 2 to power of 53)
+// string => "abc"
+// boolean => true
+// null  => standalone value
+// undefined => undefined
+// symbol  => unique
+
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null)
