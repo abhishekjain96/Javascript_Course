66caac8901f70caf2cd0f781c6019895e2f359d7
Abhishek Jain <123144040+abhishekjain96@users.noreply.github.com>
2023-12-07 04:33:47 +0000

    Discuss Tricky questions of comparision Operators

---
+++ 01_Basic/03_comparison.js
@@ -0,0 +1,39 @@
+// console.log(2>2);
+// console.log(3<2)
+// console.log(2==2)
+// console.log(2>=2)
+// console.log(2<=2)
+// console.log(2!=2)
+
+// Confusion questions
+
+console.log("2" > 1); // true
+console.log("2"<1);    // false
+console.log("2">=0);  // true
+console.log("2"<=0)   // false
+console.log("2"!=0)   // true
+
+// Null questions
+
+let ab = null
+
+let value = Number(ab)
+console.log(value)  // 0
+
+console.log(null>0) // false
+console.log(null<0)   // false 
+console.log(null>=0)   // true
+console.log(null<=0)   // true
+console.log(null!=1)   //false
+
+// Undefined
+let aj = undefined
+let ve = Number(aj)
+console.log(ve)
+
+
+console.log(undefined>0) // false
+console.log(undefined<0)   // false 
+console.log(undefined>=0)   // true
+console.log(undefined<=0)   // true
+console.log(undefined!=1) 
